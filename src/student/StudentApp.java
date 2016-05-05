f636aa7b1269734ce05f339d5b4e6fe2e027b8ae
Chayanin Punjakunaphorn <Chayanin Punjakunaphorn>
2016-05-06 02:04:56 +0700

    set indent

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,4 +1,5 @@
 package student;
+
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.function.Predicate;
 
 /**
  * Display reminders of students having a birthday soon.
+ * 
  * @author Chayanin Punjakunaporn
  */
 public class StudentApp {
@@ -14,28 +16,33 @@ public class StudentApp {
 	/**
 	 * Print the names (and birthdays) of students having a birthday in the
 	 * specified month.
-	 * @param students list of students
-	 * @param filter that test that who of student has birthday in the month of today
+	 * 
+	 * @param students
+	 *            list of students
+	 * @param filter
+	 *            that test that who of student has birthday in the month of
+	 *            today
 	 */
 	public void filterAndPrint(List<Student> students, Predicate<Student> filter, Consumer<Student> action, Comparator<Student> byBirthday) {
 		students.stream().filter(filter).sorted(byBirthday).forEach(action);
 	}
-	
+
 	/**
-	 * Run this class.
-	 * Calling filterAndPrint is example, so predicate, consumer, and comparator that be used all are ready.
+	 * Run this class. Calling filterAndPrint is example, so predicate,
+	 * consumer, and comparator that be used all are ready.
 	 */
 	public static void main(String[] args) {
 		List<Student> students = Registrar.getInstance().getStudents();
 		StudentApp app = new StudentApp();
 		Predicate<Student> checkBirthMonth = (s) -> (s.getBirthdate().getMonthValue() == LocalDate.now().getMonthValue());
-		Predicate<Student> checkBirthdayIn2Week = (s) -> (s.getBirthdate().getDayOfYear()-LocalDate.now().getDayOfYear() <= 14 && s.getBirthdate().getDayOfYear()-LocalDate.now().getDayOfYear() >= 0);
-		Consumer<Student> studentConsumer = (s) -> System.out.println(s.toString()+" will have birthday on "+
-														s.getBirthdate().getDayOfMonth()+" "+
-														s.getBirthdate().getMonth().toString());
-		Comparator<Student> byName = (a, b) -> (a.toString().compareTo(b.toString()));
-		Comparator<Student> byBirthday = (a, b) -> (a.getBirthdate().getDayOfYear()-(b.getBirthdate().getDayOfYear()));
-		
+		Predicate<Student> checkBirthdayIn2Week = (s) -> (s.getBirthdate().getDayOfYear() - LocalDate.now().getDayOfYear() <= 14
+															&& s.getBirthdate().getDayOfYear() - LocalDate.now().getDayOfYear() >= 0);
+		Consumer<Student> studentConsumer = (s) -> System.out.println(s.toString() + " will have birthday on "
+													+ s.getBirthdate().getDayOfMonth() + " "
+													+ s.getBirthdate().getMonth().toString());
+		Comparator<Student> byName = (a, b) -> a.toString().compareTo(b.toString());
+		Comparator<Student> byBirthday = (a, b) -> (a.getBirthdate().getDayOfYear() - b.getBirthdate().getDayOfYear());
+
 		app.filterAndPrint(students, checkBirthMonth, studentConsumer, byBirthday);
 	}
 }
